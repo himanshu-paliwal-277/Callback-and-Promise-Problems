71a656cae6e1b5b8f24e51c079d8d059df7d449f
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-02-07 19:51:56 +0530

    Problem 10 solution is added

---
+++ assignment_1_solution/problem_10.js
@@ -0,0 +1,20 @@
+// Problem 10:
+
+setTimeout(() => console.log("A"), 2500);
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log("B" + i);
+        setTimeout(() => {
+            console.log("C" + i);
+        }, i * 500 + 500);
+    }, i * 1000);
+}
+
+// Output:
+// B0
+// C0
+// B1
+// B2
+// C1
+// A
+// C2
