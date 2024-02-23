d0e77c7d529b4fd071dcb3ccb1ff46144ff3e7d6
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-02-23 09:08:16 +0530

    Assignment-4 question 6 solution updated

---
+++ assignment 4/Question_6.js
@@ -0,0 +1,10 @@
+// Question 6:
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Resolved!"), 1000);
+});
+promise.then((value) => console.log(value));
+console.log("After promise");
+
+// Output:
+// After promise
+// Resolved!
