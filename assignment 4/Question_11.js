710fd58415efe6360ac252caa655edc2a87e3f1f
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-02-25 08:31:50 +0530

    Assignment-4 question 11 solution is added

---
+++ assignment 4/Question_11.js
@@ -0,0 +1,7 @@
+// Question 11:
+let promise1 = Promise.resolve("First");
+let promise2 = Promise.resolve("Second");
+Promise.race([promise1, promise2]).then((value) => console.log(value));
+
+// Output:
+// First
